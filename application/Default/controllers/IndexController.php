a143433209f4e1b6cb64906235274ff86d84cd3c
josh <josh.ribakoff@gmail.com>
2013-05-26 13:48:33 -0400

    Added pagination & filtering

--- application/Default/controllers/IndexController.php
+++ application/Default/controllers/IndexController.php
@@ -5,20 +5,36 @@ class IndexController extends Zend_Controller_Action
     function indexAction()
     {
         $this->view->request = $this->getRequest();
-        $this->view->selected_vehicle = $this->flexibleSearch()->vehicleSelection()->getFirstVehicle();
-        $this->view->vehicles_list = $this->vehicleFinder()->findAll(50);
+        $this->view->selected_vehicle = $this->selection();
+        $this->view->count = $this->count();
+        $this->view->vehicles_list = $this->listVehicles();
+        $this->view->paginator = $this->paginator();
+    }
+
+    function selection()
+    {
+        return $this->flexibleSearch()
+            ->vehicleSelection()
+            ->getFirstVehicle();
     }
 
     function hasVehicleSelection()
     {
-        return !$this->flexibleSearch()->vehicleSelection()->isEmpty();
+        return !$this->flexibleSearch()
+            ->vehicleSelection()
+            ->isEmpty();
     }
 
-    function vafProductIds()
+    function listVehicles()
     {
-        $this->flexibleSearch()->storeFitInSession();
-        $productIds = $this->flexibleSearch()->doGetProductIds();
-        return $productIds;
+        if(!$this->selection()) {
+            return $this->vehicleFinder()
+                ->findAll($this->perPage(), $this->offset());
+        }
+
+        $params = $this->selection()->toValueArray();
+        return $this->vehicleFinder()
+            ->findByLevelIds($params, false, $this->perPage(), $this->offset());
     }
 
     /** @return VF_FlexibleSearch */
@@ -33,5 +49,40 @@ class IndexController extends Zend_Controller_Action
     {
         $finder = new VF_Vehicle_Finder(new VF_Schema());
         return $finder;
+    }
+
+    function paginator()
+    {
+        $adapter = new Zend_Paginator_Adapter_Null($this->count());
+        $paginator = new Zend_Paginator($adapter);
+        $paginator->setItemCountPerPage($this->perPage());
+        $paginator->setCurrentPageNumber($this->page());
+        return $paginator;
+    }
+
+    function count()
+    {
+        if(!$this->selection()) {
+            return $this->vehicleFinder()->countAll();
+        }
+
+        $params = $this->selection()->toValueArray();
+        return $this->vehicleFinder()
+            ->countByLevelIds($params);
+    }
+
+    function offset()
+    {
+        return $this->page() * $this->perPage() - $this->perPage();
+    }
+
+    function perPage()
+    {
+        return 50;
+    }
+
+    function page()
+    {
+        return $this->_getParam('page',1);
     }
 }
